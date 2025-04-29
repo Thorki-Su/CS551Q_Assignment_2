2cc67d930be378e76306c35f087e82a36d40689d
PEIHENG SU <p.su.24@abdn.ac.uk>
2025-04-29 17:30:43 +0000

    update the urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/meteorite/', permanent=True)),
     path('meteorite/', include('meteorite.urls')),
     path('user/', include('user.urls')),
     path('order/', include('order.urls')),
