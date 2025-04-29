aa582df2ddb52d3d9f721b2cb1e0737cbb1f868d
PEIHENG SU <p.su.24@abdn.ac.uk>
2025-04-29 17:00:56 +0000

    Initial commit for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-%-*hhkkr=_(=b2jm0slwx6+hq9z5z!%vjav1701yx+m#oyhfmc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sundaycinema-ericregard-8000.codio-box.uk']
+ALLOWED_HOSTS = ['sundaycinema-ericregard-8000.codio-box.uk', '*']
 
 
 # Application definition
@@ -137,10 +137,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+# Whitenoise settings
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
